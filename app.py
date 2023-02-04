6cbfeb10c27ab17503bf0fa6a0946cc24cd02713
iiitvideotagging <iiitvideotagging.project@gmail.com>
2023-02-04 15:27:31 +0530

    Trying for output path

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def videoInput(device, src, iou_score, confidence_score):
             ts = random.randrange(20, 50000, 3)  # datetime.timestamp(datetime.now())
             generated_file_name = str(ts).replace("-", "_") + uploaded_video.name
             img_path = os.path.join('data', 'uploads', generated_file_name)
-            output_path = os.path.join(Path.cwd(), 'data', 'video_output',
+            output_path = os.path.join(Path.cwd(), 'data', 'output',
                                        generated_file_name)  # os.path.basename(img_path))
 
             with open(img_path, mode='wb') as f:
--- detect.py
+++ detect.py
@@ -89,7 +89,7 @@ def detect(
 
     # Directories
     # RAHUL
-    save_dir = ROOT / 'data/video_output'
+    save_dir = ROOT / 'data/output'
     # save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
     # (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
